a3692f785bbbf49a1d5a15753bdc941f37f6c9d1
Mahmoud <mahmoud.khaled@badrit.com>
2011-08-19 02:01:08 +0200

    client side email validation

--- public/javascripts/rails.validations.js
+++ public/javascripts/rails.validations.js
@@ -402,3 +402,9 @@ var clientSideValidations = {
     }
   }
 };
+
+clientSideValidations.validators.local["email"] = function (element, options) {  
+  if(!/^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i.test(element.val())) {  
+    return options.message;  
+  }  
+}  
